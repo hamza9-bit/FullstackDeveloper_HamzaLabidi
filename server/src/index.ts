f3e25bfd940070c34e471d9494f48f897576d8ac
hamza9-bit <hamzalaabidi71@gmail.com>
2024-12-12 17:16:20 +0100

    adding rateLimiter

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,12 +2,27 @@ import express, { Request, Response } from "express";
 import productsData from "./products.json";
 import { Product } from "./types";
 import cors from "cors";
+import rateLimit from "express-rate-limit";
+
 const products: Product[] = productsData.products;
 
 const app = express();
 const port = 3000;
-app.use(cors());
-app.use(express.json());
+
+// CORS configuration to allow all origins
+app.use(cors({ origin: '*' }));
+
+// Rate limiting middleware
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per windowMs
+  message: "Too many requests from this IP, please try again later.",
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+// Apply the rate limiting middleware to all routes
+app.use(limiter);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, TypeScript with Express!");
