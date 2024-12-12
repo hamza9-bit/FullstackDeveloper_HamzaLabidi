639050060d420416769687d200a65670131569ab
hamza9-bit <hamzalaabidi71@gmail.com>
2024-12-12 17:18:00 +0100

    adding rate Limiter

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,16 +9,16 @@ const products: Product[] = productsData.products;
 const app = express();
 const port = 3000;
 
-// CORS configuration to allow all origins
+
 app.use(cors({ origin: '*' }));
 
-// Rate limiting middleware
+
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per windowMs
+  windowMs: 15 * 60 * 1000, 
+  max: 100, 
   message: "Too many requests from this IP, please try again later.",
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  standardHeaders: true, 
+  legacyHeaders: false, 
 });
 
 // Apply the rate limiting middleware to all routes
